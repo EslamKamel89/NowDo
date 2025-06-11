36365f49ea718b2240aea2e9e88f2fc7f44b6305
EslamKamel89 <elsamkamelforex@gmail.com>
2025-06-11 22:07:31 +0300

    clean up the project

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,20 +1,7 @@
-import type { LucideIcon } from 'lucide-vue-next';
 import type { Config } from 'ziggy-js';
 
 export interface Auth {
     user: User;
-}
-
-export interface BreadcrumbItem {
-    title: string;
-    href: string;
-}
-
-export interface NavItem {
-    title: string;
-    href: string;
-    icon?: LucideIcon;
-    isActive?: boolean;
 }
 
 export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
